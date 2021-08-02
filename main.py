0427242145bdcbb26a9434d0d03650fc63d457de
Rodrigo Bossini <professorbossini@gmail.com>
2021-08-02 17:30:59 -0300

    Retomar na apostila 01.02, seção 2.4.

---
+++ main.py
@@ -0,0 +1,84 @@
+x = "Estudando"
+print (id(x))
+x = x + "agora"
+#x += " agora"
+print (id(x))
+x = 2
+x = 3
+#idade = 18
+#agora sim
+#print ("Tenho " + str(idade) + " anos")
+#erro
+#print ("Tenho " + idade + " anos")
+
+
+# a = "sou um string"
+# b = 'tambem sou uma string'
+# print (type(a))
+# print(type(b))
+
+
+# print ('Estou {:-<20} matemática'.format("estudando"))
+
+# print ('{local} {atividade}'.format( atividade="estudando", local="na escola"))
+
+# local = "na escola"
+# atividade = "estudando"
+
+
+
+# print ("{0} {1}".format(local, atividade))
+# print ("{1} {0}".format(local, atividade))
+
+
+
+# disciplina = 'programação'
+# lugar = "faculdade"
+
+# print ("Cursando {} na {}".format(disciplina, lugar))
+
+
+
+# nome = "José da Silva"
+# print (f'{nome:20} é seu nome')
+# print (f'{nome:>20} é seu nome')
+# print (f'{nome:^20} é seu nome')
+
+# pi = 3.14159265359
+# print (f'Valor de pi: {pi}')
+# print (f'Valor de pi: {pi:10.6}')
+# print (f'Valor de pi: {pi:.5}')
+# print (f'Valor de pi: {pi:.6}')
+# print (f'Valor de pi: {pi:100}')
+# print (f'Valor de pi: {pi}')
+
+
+# valor1 = 2
+# valor2 = 3
+# resultado = f'{valor1} + {valor2} = {valor1 + valor2}'
+# print (resultado)
+#
+# printf ("%d + %d = %d", valor1, valor2, valor1 + valor2)
+# minha_string = f"Essa é uma string montada com f-string"
+# print (minha_string)
+
+
+
+# real = float (input('Digite um valor real'))
+# print (type(real))
+# print (real)
+
+
+# print (type(inteiro))
+# inteiro = input ("Digite um inteiro")
+# print (type(inteiro))
+# inteiro = int(inteiro)
+# print(type(inteiro))
+
+#a = input ("Digite um valor")
+#print (type(a))
+
+
+#print ("a")
+#print ("a", "b")
+#print ("a", 1, 2, "n", True)
